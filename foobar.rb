702e5a68569081c92f1768ff21b67eed2c342813
Shoumma Shams <shoumma@gmail.com>
2017-09-20 12:33:11 -0700

    Finished assignment

--- foobar.rb
+++ foobar.rb
@@ -4,10 +4,10 @@ class Foobar
     # Class method
     # Call with `Foobar.baz`
     int_array = a.map { |str| str.to_i }
-    int_array.map! { |i| i * 2 }
+    int_array.map! { |i| i + 2 }
     int_array.delete_if { |i| i % 2 == 1 }
     int_array_filt = int_array.uniq
-    int_array_filt.reject! { |i| i > 10 }
+    int_array_filt.reject! { |i| i >= 10 }
 
     sum = 0
 
